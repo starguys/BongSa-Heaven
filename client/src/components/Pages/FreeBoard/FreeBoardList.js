4721d4b0d278c60b60a0b3a91bc25fec92b963ba
starguys <kjj9500@gmail.com>
2021-11-24 21:46:32 +0900

    signIn test and devHeader devFooter

---
+++ client/src/components/Pages/FreeBoard/FreeBoardList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FreeBoardList() {
+  return <div>Im FreeBoardList</div>;
+}
---
+++ client/src/components/Pages/MyPage/UserDelete.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UserDelete() {
+  return <div>Im UserDelete</div>;
+}
