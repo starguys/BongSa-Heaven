810f6917b738ec5176da3779c79a4a26db980a76
lee inn <dpemdnjem23@gmail.com>
2021-11-29 21:47:14 +0900

    Feat: 로그인 이메일 입력 비밀번호 입력 유효성검사

--- client/src/components/Sign/SigninInput.js
+++ client/src/components/Sign/SigninInput.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from 'styled-components'
 
-
-
+import  { useState, useRef, useEffect } from 'react';
+import { Route } from "react-router-dom";
+import axios from 'axios';
 const InputBox = styled.div`
   background-color: #FFD4D4;
   width: 100%;
@@ -45,16 +46,41 @@ const PossibleOrNot = styled.div`
 
 
 
+
+
+
 export default function SignInInput() {
+
+
+  //이벤트 클릭햇을시 반응하도록
+  //이메일 입력 , 패스워드 입력
+  const [email,setEmail] = useState('')
+  const [password,setPassword] =useState('')
+
+  //로그인 버튼을 클릭햇을때
+  const handleLogin = () =>{
+  
+  }
+  const handleEmail = (e) =>{
+ 
+    console.log(e.target.value)
+    setEmail(e.target.value)
+  }
+  const handlePassword =(e) =>{
+    e.preventDefault();
+    console.log(e.target.value)
+    setPassword(e.target.value)
+  }
+
   return (
     <>
         <InputBox>
           <SignInWhiteBox>
-            <SignInWhiteInput placeholder="아이디">
+            <SignInWhiteInput type='email' placeholder="아이디(이메일)" onChange = {handleEmail} >
             </SignInWhiteInput>
           </SignInWhiteBox>
           <SignInWhiteBox>
-            <SignInWhiteInput placeholder="비밀번호">
+            <SignInWhiteInput  type='password' placeholder="비밀번호" onChange ={handlePassword}>
             </SignInWhiteInput>
           </SignInWhiteBox>
         </InputBox>
