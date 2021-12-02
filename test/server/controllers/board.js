096fd0cf97702b331b43a4b63bfe6d776b04eeec
kimVayne <nkimtnt@gmail.com>
2021-12-02 14:15:47 +0900

    20211202

--- test/server/controllers/board.js
+++ test/server/controllers/board.js
@@ -11,7 +11,32 @@ module.exports = {
     // 2. 유저가 아니라면 돌려보냄
     // 3. 유저라면 free board에 모델 생성하고 req.body로 받은 데이터 등록
 
-    return res.send("fb register ok!");
+    const authorizedUser = isAuthorized(req, res);
+    if (!authorizedUser) {
+      return res.send({ message: "싸장님 회원 맞아?? 빨리 가입 해" });
+    }
+    if (authorizedUser) {
+      const freeContent = new Freeboard({
+        user_id: req.body.user_id,
+        title: req.body.title,
+        description: req.body.description,
+        images: req.body.images,
+      });
+      freeContent
+        .save()
+        .then(() => {
+          console.log("freeboard content 등록완료!");
+        })
+        .then(() => {
+          res.status(200).send({ message: "싸장님 게시물 등록 완료!" });
+        })
+        .catch((err) => {
+          console.log("err");
+          res.status(500).send({ message: "서버가 이상해!" });
+        });
+    } else {
+      return res.status(500).send({ message: "서버 이상해!" });
+    }
   },
   fblistControl: async (req, res) => {
     return res.send("fb info ok!");
