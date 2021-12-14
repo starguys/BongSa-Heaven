00d5d132a12596486964ddc05a29b8b351c7ce4a
gitkrak <dpemdnjem23@gamail.com>
2021-12-15 01:00:22 +0900

    s

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   }),
