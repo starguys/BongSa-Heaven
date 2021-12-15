4942815ba63c800495cbf54bc8166d37508bfb4b
gitkrak <dpemdnjem23@gamail.com>
2021-12-15 22:49:36 +0900

    수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,4 +16,4 @@ export NODEMAILER_PASS=$(aws ssm get-parameters --region ap-northeast-2 --names 
 export KAKAO_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_SECRET --query Parameters[0].Value | sed 's/"//g')
 export KAKAO_CLIENT=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_CLIENT --query Parameters[0].Value | sed 's/"//g')
 
-authbind --deep pm2 start app.js
+authbind --deep pm2 start index.js
