6f5dd979b5d0ce659d762b23777a83678a8d2c79
gitkrak <dpemdnjem23@gamail.com>
2021-12-15 13:48:33 +0900

    오타 수정

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -6,4 +6,4 @@ sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
-sudo chmod 755 /etc/authbind/byport/80 o
+sudo chmod 755 /etc/authbind/byport/80
